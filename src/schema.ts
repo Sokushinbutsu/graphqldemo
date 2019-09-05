5d19a3a15b3b9fdbd9f25d312c94055b739bda6f
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-09-05 00:41:38 -0400

    graphql basic implementation

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
 const {
-  GraphQLObectType,
+  GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
@@ -7,4 +7,43 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
-module.exports = new GraphQLSchema({});
+// Hardcoded Data
+
+const customers = [
+  { id: "1", name: "john doe", email: "jdoe@gmail.com", age: 12 },
+  { id: "2", name: "jane doe", email: "janedoe@gmail.com", age: 13 },
+  { id: "3", name: "rob doe", email: "rdoe@gmail.com", age: 15 }
+];
+
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
